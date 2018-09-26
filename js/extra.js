22d5d32becb846a9fcdf7fb72931072da23e80c9
FredaChew97 <freddy_chew@outlook.com>
2018-09-26 09:11:05 +0800

    javascript files added

---
+++ js/extra.js
@@ -0,0 +1,16 @@
+
+
+//Need modification 
+function choices() {
+    var x = document.getElementById("todolist");
+    var x = document.getElementById("note");
+    
+        if (onclick(todoList)==true){
+
+        }
+
+        else(onclick(note)==true){
+
+        }
+   
+}
